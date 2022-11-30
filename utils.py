dae523a7e11a2f9ccf527efac635b202d542a300
nnnoopt <nnnoopt@yandex.ru>
2022-11-30 15:42:09 +0300

    bugs fixed in total_base

--- utils.py
+++ utils.py
@@ -31,6 +31,6 @@ class Converter:
             raise ConvertExeption(f"Не удалось обработать {amount}")
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
+        total_base = json.loads(r.content)[keys[base]]*amount
 
         return total_base
